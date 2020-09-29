fb3a472b3478d24955e4c3fa952b0cf2c60ba742
Przemad <adamowicz@helloweb.pl>
2020-09-29 18:15:56 +0200

    remove bootsnap on production

--- config/boot.rb
+++ config/boot.rb
@@ -1,16 +1,22 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require "bootsnap"
 
 env = ENV["RAILS_ENV"] || "development"
 
-Bootsnap.setup(
-  cache_dir: File.expand_path(File.join("..", "tmp", "cache"), __dir__),
-  development_mode: env == "development",
-  load_path_cache: true,
-  autoload_paths_cache: true,
-  disable_trace: false,
-  compile_cache_iseq: !ENV["SIMPLECOV"],
-  compile_cache_yaml: true
-)
+begin
+  require "bootsnap"
+  Bootsnap.setup(
+    cache_dir: File.expand_path(File.join("..", "tmp", "cache"), __dir__),
+    development_mode: env == 'development',
+    load_path_cache: true,
+    autoload_paths_cache: true,
+    disable_trace: false,
+    compile_cache_iseq: !ENV['SIMPLECOV'],
+    compile_cache_yaml: true
+  )
+rescue LoadError
+  # Do not load in production because file system (where cache would be written) is read-only
+  nil
+end
+
